d3c70ba84f7768ead64277f1a7be062fdcc74e14
trubiv <i.trubchaninov@lovata.com>
2023-06-06 15:10:28 +0300

    add laravel with bootstrap admin panel

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,24 @@
+</div>
+        <!-- /.content-wrapper -->
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.2.0
+            </div>
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
---
+++ src/resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('page')
+
+@section('title', 'first')
+
+@section('content')
+<p>This is my body content.</p>
+@endsection
