fb325a20d3646648342103862d26446c373c5692
ssssory <itrubchaninov@lisr.ru>
2023-07-25 01:28:23 +0300

    fix table

--- src/resources/views/pages/rent-table.blade.php
+++ src/resources/views/pages/rent-table.blade.php
@@ -17,8 +17,8 @@
                     <th>description</th>
                     <th>images</th>
                     <th>address</th>
-                    <th>phone</th>
-                    <th>url</th>  
+                    <th>url</th>
+                    <th>action</th>
                 </tr>
             </thead>
             <tbody>
@@ -27,14 +27,17 @@
                     <td>{{$loop->index}}</td>
                     <td>{{$model->name}}</td>
                     <td>{{$model->description}}</td>
-                    <td>
-                    @foreach(explode(',',$model->images) as $image)
-                        <img src="{{$image}}" width="100px">
-                    @endforeach
-                    </td>
+                    <td>{{$model->price}}</td>
                     <td>{{$model->address}}</td>
-                    <td>{{$model->phone}}</td>
                     <td>{{$model->url}}</td>
+                    <td>{{$model->phone}}</td>
+                </tr>
+                <tr>
+                    <td colspan="7">
+                        @foreach(explode(',',$model->images) as $image)
+                        <img src="{{$image}}" width="200px">
+                        @endforeach
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
