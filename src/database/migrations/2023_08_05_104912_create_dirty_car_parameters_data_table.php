98b1d2079aee91b955b885b103ccee772a651868
ssssory <itrubchaninov@lisr.ru>
2023-08-05 14:57:20 +0300

    add car parameters table

---
+++ src/database/migrations/2023_08_05_104912_create_dirty_car_parameters_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dirty_car_parameters_data', function (Blueprint $table) {
+            $table->id();
+            $table->integer('car_id');
+            $table->string('property');
+            $table->string('name')->nullable();
+            $table->string('value');
+            $table->boolean('is_appruved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dirty_car_parameters_data');
+    }
+};
