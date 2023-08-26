6b3717777575c8458d642c30df0801ce362d6ee4
ssssory <itrubchaninov@lisr.ru>
2023-08-26 21:57:29 +0300

    add topics to sender service

--- src/app/Services/SenderService.php
+++ src/app/Services/SenderService.php
@@ -17,7 +17,7 @@ class SenderService
     )
     {}
 
-    function sendTelegram(MessageCar $messageCar, string $chatId=null, $type= self::AUTO): void
+    function sendTelegram(MessageCar $messageCar, string $chatId=null, string $type= self::AUTO, ?int $topic= null): void
     {
         CompleteMessage::create([
             'model' => $messageCar->original::class,
@@ -26,6 +26,6 @@ class SenderService
             'chat' => $chatId??Telegram::TEST_CHAT_ID,
             'type' => $type,
         ]);
-        $this->telegram->sendMediaMessage($messageCar, $chatId);
+        $this->telegram->sendMediaMessage($messageCar, $chatId, $topic);
     }
 }
