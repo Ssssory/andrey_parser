702a16f62de71d2d79ac2313fd9f08fd1ffd0043
Ssssory <itrubchaninov@list.ru>
2023-09-20 20:17:53 +0300

    update send service

--- src/app/Services/SenderService.php
+++ src/app/Services/SenderService.php
@@ -2,23 +2,62 @@
 
 namespace App\Services;
 
+use App\Classes\Dto\Message;
 use App\Classes\Messages\MessageCar;
+use App\Classes\Storages\TelegramStorage;
 use App\Classes\Telegram\Telegram;
+use App\Enums\SendType;
+use App\Enums\SourceType;
+use App\Enums\Transport;
 use App\Models\CompleteMessage;
 use Carbon\CarbonImmutable;
 
 class SenderService
 {
-    const HANDLE = 'handle';
-    const AUTO = 'auto';
+    private SendType $sendType;
+    private SourceType $sourceType;
+    private Transport $transport;
 
     function __construct(
         private MessageService $messageService,
-        private Telegram       $telegram
+        private TelegramStorage $telegramStorage,
     )
-    {}
+    {
+    }
 
-    function sendTelegram(MessageCar $messageCar, string $chatId=null, string $type= self::AUTO, ?int $topic= null): void
+    function init(SendType $sendType, Transport $transport, SourceType $sourceType): void
+    {
+        $this->sendType = $sendType??SendType::Auto;
+        $this->sourceType = $sourceType??SourceType::Car;
+        $this->transport = $transport??Transport::Telegram;
+        if ($transport->value == Transport::Telegram->value) {
+            $this->telegramStorage->make($this->sourceType);
+        }
+
+    }
+
+    public function sendMessage(Message $config): void
+    {
+        $transportStorage = $this->getTransport($config->transport);
+        if ($transportStorage == null) {
+            return;
+        }
+        if (!$transport = $transportStorage->getReady()) {
+            return;
+        }
+
+        CompleteMessage::create([
+            'model' => $config->message->original::class,
+            'model_id' => $config->message->original->id,
+            'message' => $config->message::class,
+            'chat' => $config->target[0],
+            'type' => $this->sendType,
+        ]);
+
+        $transport->sendMediaMessage($config->message, $config->target[0], $config->target[1]);
+    }
+
+    function sendTelegram(MessageCar $messageCar, string $chatId=null, string $type= SendType::Auto, ?int $topic= null): void
     {
         CompleteMessage::create([
             'model' => $messageCar->original::class,
@@ -27,7 +66,7 @@ class SenderService
             'chat' => $chatId??Telegram::TEST_CHAT_ID,
             'type' => $type,
         ]);
-        $this->telegram->sendMediaMessage($messageCar, $chatId, $topic);
+        $this->telegramStorage->getReady()->sendMediaMessage($messageCar, $chatId, $topic);
     }
 
     function isTimeToSend(): bool
@@ -39,5 +78,15 @@ class SenderService
             return true;
         }
         return false;
+    }
+
+    private function getTransport(Transport $transport)
+    {
+        switch ($transport->value) {
+            case Transport::Telegram->value:
+                return $this->telegramStorage;
+            default:
+                return null;
+        }    
     }
 }
